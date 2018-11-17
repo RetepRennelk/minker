c06d4f2dfbeb7602f675292fa08aa81682161315
RetepRennelk <peterklenner@gmx.de>
2018-11-17 16:55:56 +0100

    Added TableWidget as central widget.

--- minker/minker.py
+++ minker/minker.py
@@ -1,12 +1,34 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon  
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidget,\
+    QHeaderView
+from PyQt5.QtGui import QIcon
+
+
+class TableWidget(QTableWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        header = self.horizontalHeader()
+        header.setSectionResizeMode(QHeaderView.Stretch)
+
+        self.setColumnCount(2)
+        self.setRowCount(1)
+
+        labels = ["Original", "Transmogrified"]
+        self.setHorizontalHeaderLabels(labels)
+
+
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Minker")
         self.setWindowIcon(QIcon('list.png'))
+
+        tw = TableWidget(self)
+        self.setCentralWidget(tw)
+        
 
 
 if __name__ == '__main__':
