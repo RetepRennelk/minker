afed4b83f47660b179995ec68bbc8c22e784f80b
RetepRennelk <peterklenner@gmx.de>
2018-11-18 08:53:24 +0100

    Styled main window and increased font size.

--- minker/minker.py
+++ minker/minker.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5.QtGui import QIcon
+from PyQt5.QtGui import QIcon, QFont
 from tablewidget import TableWidget
 
 
@@ -15,7 +15,20 @@ class MainWindow(QMainWindow):
 
 
 if __name__ == '__main__':
+    sCSS = '''
+    QTableWidget {
+        gridline-color: black;
+    }
+    QHeaderView::section {
+        background-color: black;
+        color: white
+    }
+    '''
     app = QApplication(sys.argv)
+    app.setStyleSheet(sCSS)
+    app.setFont(QFont("Ubuntu Mono", 16))
     m = MainWindow()
+    m.setGeometry(200, 200, 600, 809)
     m.show()
     app.exec_()
+
